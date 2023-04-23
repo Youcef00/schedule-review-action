ba63cad1b725642d0ef94fd117d4132da691d5da
Youcef MOUKEUT <youcef.moukeut@decathlon.com>
2023-04-23 08:27:08 +0200

    Squashed all previous commits

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # schedule-review-action
+
+This action sends a Google Chat message to the specified space notified the specified user of his pending reviews
+
+## Inputs
+
+### `webhooks`
+
+**Required** A list of Google chat webhooks to the users, the format must be the following:
+
+`git_username:webhook_URL git_username2:webhook_URL2`
+
+### `token`
+
+**Required** A GitHub token for the GitHub API. 
+*Note*: It is recommended to the use the GitHub Action's token and enabling read/write access in the repository settings under `Settings > Actions > General > Workflow permissions`  
+
+## Example usage
+
+```yaml
+on:
+  pull_request:
+    types: [ review_requested ]
+
+jobs:
+  scheduled-PR-notification:
+    runs-on: ubuntu-latest
+    steps:
+      - name: CR list publication
+        id: cr_list
+        uses: Youcef00/schedule-review-action@v1.0
+        with:
+          webhooks: ${{ format('{0} {1}', secrets.WEBHOOK_GITBOT, secrets.WEBHOOK_PLATCHOON) }}
+          token: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,103 @@
+import core from '@actions/core';
+import fetch from 'node-fetch';
+import github from '@actions/github';
+import { Octokit } from "octokit";
+
+/**
+ * Sends asynchronous message into Google Chat
+ * @return response
+ */
+async function sendScheduledMessage() {
+
+    const webhooks = core.getInput('webhooks').split(" ");
+    const users = webhooks.map(webhook => {
+        const user = webhook.split(/:(.*)/s)[0];
+        const webhookURL = webhook.split(/:(.*)/s)[1];
+        return {username: user, webhook: webhookURL, pull_requests: []};
+    });
+
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const repository = core.getInput('repository');
+
+    const octokit = new Octokit({
+        auth: token
+    });
+    const response = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+    });
+
+    if (response.status !== 200) {
+        throw new Error(`GitHub API responded with status ${response.status}`);
+    }
+
+    const pull_requests = response.data;
+    pull_requests.forEach(pr => {
+        for (let user of users) {
+            const reviewers = pr.requested_reviewers.map(reviewer => reviewer.login);
+            if (reviewers.includes(user.username)) {
+                user.pull_requests.push(pr);
+            }
+        }
+    });
+
+    sendMessage(users);
+
+}
+
+const generateMessage = (user) => {
+    return `<p>You have <b><font color="#D14F0A">${user.pull_requests.length}</font></b> pending review requests!</p>
+        ${user.pull_requests.map(pr => `<p>🚨 <a href=${pr.html_url}>PR-${pr.number}</a></p>`).join('<br>')}`;
+}
+
+
+/**
+ * Sends asynchronous message into Google Chat
+ * @return response
+ */
+function sendMessage(users) {
+    users.forEach(user => {
+        try {
+            let data = {
+                "cards": [
+                    {
+                        "header": {
+                            "title": "⚠️️ Pending code reviews reminder  ⚠️"
+                        },
+                        "sections": [
+                            {
+                                "widgets": [
+                                    {
+                                        "textParagraph": {
+                                            "text": `${generateMessage(user)}`
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            };
+            data = JSON.stringify(data);
+            let resp;
+            fetch(user.webhook, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                body: data,
+            }).then((response) => {
+                resp = response;
+                console.log(`POST sent, Response: ${response.statusText}`);
+            });
+        } catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+
+
+
+}
+
+sendScheduledMessage();
